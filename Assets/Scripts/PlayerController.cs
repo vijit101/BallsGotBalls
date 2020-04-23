293fe745d1e18801dd6bed370b38722a73bafcbc
Shubham <vijit101@gmail.com>
2020-04-24 00:39:47 +0530

    - Day 1 - Player & Camera Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rgbd;
+    public float speed = 10;
+    public GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        rgbd.AddForce(focalPoint.transform.forward*speed*forwardInput);
+    }
+}
