e818f6e93e89c978362b3781278c17157e9cdb9e
Shubham <vijit101@gmail.com>
2020-04-25 01:00:17 +0530

    - Added Enemies -enemy follow -spwan manager

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rgbd;
+    GameObject player;
+    public float chasingSpeed = 1;
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody>();
+        player = PlayerController.instance.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        rgbd.AddForce(lookDirection*chasingSpeed);
+        // normalized keeps the distance of vector but clamp the value to 1 so now enemy will always come at player with same speed as 
+        // now if distance btw player and enemy is 10 units or 2 units force added is 1 as it is normalized basically a unity vector direction 
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController instance;
     Rigidbody rgbd;
     public float speed = 10;
     public GameObject focalPoint;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
